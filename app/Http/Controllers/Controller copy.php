9e5c213503a9e99e316d793f1a806c0b0bcbf88c
christian rojas collazos <crc1992_3@hotmail.com>
2022-02-22 17:48:52 -0500

    new code

---
+++ app/Http/Controllers/Controller copy.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    use ApiResponser;
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        return 'Hola Mundo'
+    }
+
+    /**
+     * Return author list
+     * @return Illuminate\Http\Response
+     */
+    public function index(){
+        $authors = Author::all();
+        $test = ['lalal','lelele','lilili'];
+        foreach ($test as $key => $value) {
+            if ($value == 'lilili') {
+                $authors = 'nice';
+            }
+        }
+        foreach ($test as $key => $value) {
+            if ($value == 'lilili') {
+                $authors = 'nice';
+            }
+        }
+        foreach ($test as $key => $value) {
+            if ($value == 'lilili') {
+                $authors = 'nice';
+            }
+        }
+        foreach ($test as $key => $value) {
+            if ($value == 'lilili') {
+                $authors = 'nice';
+            }
+        }
+
+        return $this->successResponse($authors);
+    }
+}
